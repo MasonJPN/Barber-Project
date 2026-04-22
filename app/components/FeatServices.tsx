c11c5f7036ea96d13371ced6deb00f58183815de
Mason Fancher <mason.fancher.jp@outlook.com>
2026-04-22 23:56:55 +0900

    starting work on featured cut section

---
+++ app/components/FeatServices.tsx
@@ -0,0 +1,22 @@
+
+
+
+export default function FeatServices(){
+    return (
+        <div className="bg-black">
+
+            <div>
+                <h2>Featured Services</h2>
+                <div>
+                    <ul className="underline">
+                        <li>Classic Gentleman Cut</li>
+                        <li>Buzz Cut</li>
+                        <li>Straight Razor Shave</li>
+                        <li>Razor Beard Trim</li>
+                    </ul>
+                    <h2>*Cancellations or No-Shows within 24 hours of appointment time will result in 100% charge of service.</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/components/WelcomeBar.tsx
+++ app/components/WelcomeBar.tsx
@@ -4,16 +4,16 @@
 export default function WelcomeBar(){
 
     return (
-        <div className="flex px-10 py-10 gap-20">
+        <div className="flex px-10 py-10 gap-10">
             <div className="flex flex-col gap-4 justify-center">
-                <h2 className="mb-10 text-4xl font-semibold">WELCOME TO MY SHOP</h2>
+                <h2 className="mb-5 text-4xl font-semibold">WELCOME TO MY BARBER SHOP</h2>
                 <h3 className="text-xl">Stylish and modern haircuts, here you can get a clean, classic look.</h3>
                 <p className="text-xl">Hair is our passion, and our passion shows on every client that walks out of our doors. Through ongoing education, our Chicago stylists stay up to date on the latest hairstyles for men and styles and techniques to bring you the looks you want.</p>
             </div>
 
             <div className="">
                 <img 
-                    className="h-90 w-300"
+                    className="h-80 w-500"
                     src="/DefaultHero.png"/>
             </div>
         </div>
